980e1640298fafaf4a13bfc14f7f00f46f8d2f4c
BlueIceBear <fbcats2@gmail.com>
2017-04-01 23:40:06 +0100

    Updated code - new functions

---
+++ 1024.c
@@ -0,0 +1,559 @@
+#include <SDL2/SDL.h>
+#include <SDL2/SDL_ttf.h>
+#include <SDL2/SDL_image.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+#include <stdbool.h>
+
+#define STRING_SIZE 100       // max size for some strings
+#define TABLE_SIZE 650        // main game space size
+#define LEFT_BAR_SIZE 150     // left white bar size
+#define WINDOW_POSX 200       // initial position of the window: x
+#define WINDOW_POSY 200       // initial position of the window: y
+#define SQUARE_SEPARATOR 8    // square separator in px
+#define BOARD_SIZE_PER 0.7f   // board size in % wrt to table size 
+#define MAX_BOARD_POS 11      // maximum size of the board
+#define MAX_LEVELS 23
+#define MARGIN 5
+
+// declaration of the functions related to graphical issues
+void InitEverything(int , int , TTF_Font **, SDL_Surface **, SDL_Window ** , SDL_Renderer ** );
+void InitSDL();
+void InitFont();
+SDL_Window* CreateWindow(int , int );
+SDL_Renderer* CreateRenderer(int , int , SDL_Window *);
+int RenderText(int, int, const char *, TTF_Font *, SDL_Color *, SDL_Renderer *);
+int RenderLogo(int, int, SDL_Surface *, SDL_Renderer *);
+void RenderTable(int, int *, int *, TTF_Font *, SDL_Surface **, SDL_Renderer *);
+void RenderBoard(int [][MAX_BOARD_POS], SDL_Surface **, int, int, int, SDL_Renderer *);
+void RenderStats( SDL_Renderer *, int , int , int );
+void LoadValues(SDL_Surface **);
+void UnLoadValues(SDL_Surface **);
+
+void Parameters(int *, char *, int*);
+void SaveResults(char * , int, int, int);
+void NewGame();
+
+// definition of some strings: they cannot be changed when the program is executed !
+const char myName[] = "Francisco Branco";
+const char myNumber[] = "IST425347";
+
+/**
+ * main function: entry point of the program
+ * only to invoke other functions !
+ */
+int main( int argc, char* args[] )
+{
+    SDL_Window *window = NULL;
+    SDL_Renderer *renderer = NULL;
+    TTF_Font *serif = NULL;
+    SDL_Surface *array_of_numbers[MAX_LEVELS], *imgs[2];
+    SDL_Event event;
+    int delay = 300;
+    int quit = 0;
+    int width = (TABLE_SIZE + LEFT_BAR_SIZE);
+    int height = TABLE_SIZE;
+    int square_size_px, board_size_px, board_pos = 5;
+    int board[MAX_BOARD_POS][MAX_BOARD_POS] = {{0}};
+
+    int size;
+    char name[STRING_SIZE];
+    int difficulty;
+
+    int points = 0, max_points = 0, max_pc = 0, game_time = 0;
+
+
+    // some examples
+    board[0][0] = 1;
+    board[2][0] = 2;
+    board[3][3] = 3;
+    board[4][4] = 4;
+    board[4][0] = 5;
+    board[3][0] = 8;
+    board[0][1] = 10;
+
+    // initialize graphics
+    InitEverything(width, height, &serif, imgs, &window, &renderer);
+    // loads numbers as images
+    LoadValues(array_of_numbers);
+
+    Parameters(&size, name, &difficulty);
+
+
+    while( quit == 0 )
+    {
+        // while there's events to handle
+        while( SDL_PollEvent( &event ) )
+        {
+            if( event.type == SDL_QUIT )
+            {
+                quit = 1;   // quit !
+            }
+            else if ( event.type == SDL_KEYDOWN )
+            {
+                switch ( event.key.keysym.sym )
+                {
+                    case SDLK_q:
+                        quit = 1;
+                        break;
+
+                    case SDLK_n:
+                        NewGame();
+
+                    case SDLK_u:
+
+                    case SDLK_UP:
+                        // todo
+                    case SDLK_DOWN:
+                        // todo
+                    case SDLK_LEFT:
+                        // todo
+                    case SDLK_RIGHT:
+                        // todo
+                    default:
+                        break;
+                }
+            }
+        }
+        // render game table
+        RenderTable( board_pos, &board_size_px, &square_size_px, serif, imgs, renderer);
+        // render board 
+        RenderBoard(board, array_of_numbers, board_pos, board_size_px, square_size_px, renderer);
+        // render in the screen all changes above
+        SDL_RenderPresent(renderer);
+        // add a delay
+        SDL_Delay( delay );
+    }
+
+    // free memory allocated for images and textures and closes everything including fonts
+    UnLoadValues(array_of_numbers);
+    TTF_CloseFont(serif);
+    SDL_FreeSurface(imgs[0]);
+    SDL_FreeSurface(imgs[1]);
+    SDL_DestroyRenderer(renderer);
+    SDL_DestroyWindow(window);
+    SDL_Quit();
+
+    SaveResults(name, points, max_pc, game_time);
+
+    return EXIT_SUCCESS;
+}
+
+
+void Parameters(int *_size, char *_name, int *_difficulty)
+{
+    int var = 0;
+
+    int size = *_size, difficulty = *_difficulty;
+
+    while(var == 0)
+    {
+        printf("Choose size for board: ");
+        scanf("%d", &size);
+
+        if(size < 2 || size > 11) printf("The board size must be between 2 and 11.\n");
+        else var = 1;
+    }
+
+    var = 0;
+
+    while(var == 0)
+    {
+        printf("\nChoose your name: ");
+        scanf("%s", _name);
+
+        if(strlen(_name) > 8 || strlen(_name) < 1) printf("The name must have at least 8 characters.\n");
+        else var = 1;
+    }
+
+    var = 0;
+
+    while(var == 0)
+    {
+        printf("\nChoose difficulty (from 4 to 23): ");
+        scanf("%d", &difficulty);
+
+        if(difficulty < 4 || difficulty > 23) printf("The difficulty must be between 4 and 23.\n");
+        else var = 1;
+    }
+
+    *_size = size;
+    *_difficulty = difficulty;
+}
+
+void NewGame()
+{
+    
+}
+
+
+void SaveResults(char *name, int points, int max_pc, int game_time)
+{
+    FILE *results;
+
+    results = fopen("stats.txt", "w");
+
+    fprintf(results, "%s\nPoints - %d\nHighest piece - %d\nTime - %d", name, points, max_pc, game_time);
+
+    fclose(results);
+}
+
+
+/**
+ * LoadCards: Loads all images with the values
+ * \param _array_of_numbers vector with all loaded numbers
+ */
+void LoadValues(SDL_Surface **_array_of_numbers)
+{
+    int i;
+    char filename[STRING_SIZE];
+
+    // loads all images to an array
+    for ( i = 0 ; i < MAX_LEVELS; i++ )
+    {
+        // create the filename !
+        sprintf(filename, ".//numbers//value_%02d.jpg", i+1);
+        // loads the image !
+        _array_of_numbers[i] = IMG_Load(filename);
+        // check for errors: deleted files ?
+        if (_array_of_numbers[i] == NULL)
+        {
+            printf("Unable to load image: %s\n", SDL_GetError());
+            exit(EXIT_FAILURE);
+        }
+    }
+}
+
+
+/**
+ * UnLoadCards: unloads all values images of the memory
+ * \param _array_of_numbers vector with all loaded image numbers
+ */
+void UnLoadValues(SDL_Surface **_array_of_numbers)
+{
+    // unload all cards of the memory: +1 for the card back
+    for ( int i = 0 ; i < MAX_LEVELS; i++ )
+    {
+        SDL_FreeSurface(_array_of_numbers[i]);
+    }
+}
+
+/**
+ * RenderBoard: renders the board
+ * \param _board multidimensional array with the board numbers
+ * \param _array_of_numbers vector with all loaded images
+ * \param _board_pos number of positions in the board
+ * \param _board_size_px size of the board in pixels
+ * \param _square_size_px size of each square
+ * \param _renderer renderer to handle all rendering in a window
+ */
+void RenderBoard(int _board[][MAX_BOARD_POS], SDL_Surface **_array_of_numbers, int _board_pos,
+        int _board_size_px, int _square_size_px, SDL_Renderer *_renderer )
+{
+    int x_corner, y_corner;
+    SDL_Rect boardPos;
+    SDL_Texture *number_text;
+
+    // corner of the board
+    x_corner = (TABLE_SIZE - _board_size_px) >> 1;
+    y_corner = (TABLE_SIZE - _board_size_px - 15);
+
+    // renders the squares where the numbers will appear
+    for ( int i = 0; i < _board_pos; i++ )
+    {
+        for ( int j = 0; j < _board_pos; j++ )
+        {
+            // basic check
+            if ( _board[i][j] > 23 || _board[i][j] < 0 ) 
+            {
+                printf("Invalid board value: RenderBoard\n");
+                exit(EXIT_FAILURE);
+            }
+            // only draws a number if it corresponds to a non-empty space
+            if ( _board[i][j] != 0 )
+            {
+                // define the size and copy the image to display
+                boardPos.x = x_corner + (i+1)*SQUARE_SEPARATOR + i*_square_size_px;  
+                boardPos.y = y_corner + (j+1)*SQUARE_SEPARATOR + j*_square_size_px;
+                boardPos.w = _square_size_px;
+                boardPos.h = _square_size_px;
+                number_text = SDL_CreateTextureFromSurface( _renderer, _array_of_numbers[ _board[i][j] - 1] );
+                SDL_RenderCopy( _renderer, number_text, NULL, &boardPos );
+                // destroy texture 
+                SDL_DestroyTexture(number_text);
+            }
+        }
+    }
+}
+
+/*
+ * Shows some information about the game:
+ * - Level to achieve (difficulty)
+ * - Score of the game
+ * - Time spent in seconds
+ * \param _renderer renderer to handle all rendering in a window
+ * \param _level current level
+ * \param _score actual score
+ * \param _time time to display in seconds
+ */
+void RenderStats( SDL_Renderer *_renderer, int _level, int _score, int _time )
+{
+    /* To Be Done */
+}
+
+/*
+ * RenderTable: Draws the table where the game will be played, namely:
+ * -  some texture for the background
+ * -  the right part with the IST logo and the student name and number
+ * -  the grid for game board with squares and seperator lines
+ * \param _board_pos number of squares in the board 
+ * \param _font font used to render the text
+ * \param _img surfaces with the table background and IST logo (already loaded)
+ * \param _renderer renderer to handle all rendering in a window
+ */
+void RenderTable( int _board_pos, int *_board_size_px, int *_square_size_px, TTF_Font *_font, SDL_Surface *_img[], SDL_Renderer* _renderer )
+{
+    SDL_Color black = { 0, 0, 0 }; // black
+    SDL_Color light = { 205, 193, 181 };
+    SDL_Color dark = { 120, 110, 102 };
+    SDL_Texture *table_texture;
+    SDL_Rect tableSrc, tableDest, board, board_square;
+    int height, board_size_px, square_size_px;
+
+    // set color of renderer to some color
+    SDL_SetRenderDrawColor( _renderer, 255, 255, 255, 255 );
+
+    // clear the window
+    SDL_RenderClear( _renderer );
+
+    tableDest.x = tableSrc.x = 0;
+    tableDest.y = tableSrc.y = 0;
+    tableSrc.w = _img[0]->w;
+    tableSrc.h = _img[0]->h;
+    tableDest.w = TABLE_SIZE;
+    tableDest.h = TABLE_SIZE;
+
+    // draws the table texture
+    table_texture = SDL_CreateTextureFromSurface(_renderer, _img[0]);
+    SDL_RenderCopy(_renderer, table_texture, &tableSrc, &tableDest);
+
+    // render the IST Logo
+    height = RenderLogo(TABLE_SIZE, 0, _img[1], _renderer);
+
+    // render the student name
+    height += RenderText(TABLE_SIZE+3*MARGIN, height, myName, _font, &black, _renderer);
+
+    // this renders the student number
+    RenderText(TABLE_SIZE+3*MARGIN, height, myNumber, _font, &black, _renderer);
+
+    // compute and adjust the size of the table and squares
+    board_size_px = (int)(BOARD_SIZE_PER*TABLE_SIZE);
+    square_size_px = (board_size_px - (_board_pos+1)*SQUARE_SEPARATOR) / _board_pos; 
+    board_size_px -= board_size_px % (_board_pos*(square_size_px+SQUARE_SEPARATOR));
+    board_size_px += SQUARE_SEPARATOR;
+
+    // renders the entire board background 
+    SDL_SetRenderDrawColor(_renderer, dark.r, dark.g, dark.b, dark.a );
+    board.x = (TABLE_SIZE - board_size_px) >> 1;
+    board.y = (TABLE_SIZE - board_size_px - 15);
+    board.w = board_size_px;
+    board.h = board_size_px;
+    SDL_RenderFillRect(_renderer, &board);
+
+    // renders the squares where the numbers will appear
+    SDL_SetRenderDrawColor(_renderer, light.r, light.g, light.b, light.a );
+
+    // iterate over all squares
+    for ( int i = 0; i < _board_pos; i++ )
+    {
+        for ( int j = 0; j < _board_pos; j++ )
+        {
+            board_square.x = board.x + (i+1)*SQUARE_SEPARATOR + i*square_size_px;  
+            board_square.y = board.y + (j+1)*SQUARE_SEPARATOR + j*square_size_px;
+            board_square.w = square_size_px;
+            board_square.h = square_size_px;
+            SDL_RenderFillRect(_renderer, &board_square);
+        }
+    }
+
+    // destroy everything
+    SDL_DestroyTexture(table_texture);
+    // store some variables for later use
+    *_board_size_px = board_size_px;
+    *_square_size_px = square_size_px;
+}
+
+/**
+ * RenderLogo function: Renders the IST logo on the app window 
+ * \param x X coordinate of the Logo
+ * \param y Y coordinate of the Logo
+ * \param _logoIST surface with the IST logo image to render
+ * \param _renderer renderer to handle all rendering in a window
+ */
+int RenderLogo(int x, int y, SDL_Surface *_logoIST, SDL_Renderer* _renderer)
+{
+    SDL_Texture *text_IST;
+    SDL_Rect boardPos;
+
+    // space occupied by the logo
+    boardPos.x = x;
+    boardPos.y = y;
+    boardPos.w = _logoIST->w;
+    boardPos.h = _logoIST->h;
+
+    // render it
+    text_IST = SDL_CreateTextureFromSurface(_renderer, _logoIST);
+    SDL_RenderCopy(_renderer, text_IST, NULL, &boardPos);
+
+    // destroy associated texture !
+    SDL_DestroyTexture(text_IST);
+    return _logoIST->h;
+}
+
+/**
+ * RenderText function: Renders some text on a position inside the app window
+ * \param x X coordinate of the text
+ * \param y Y coordinate of the text
+ * \param text string with the text to be written
+ * \param _font TTF font used to render the text
+ * \param _color color of the text
+ * \param _renderer renderer to handle all rendering in a window
+ */
+int RenderText(int x, int y, const char *text, TTF_Font *_font, SDL_Color *_color, SDL_Renderer* _renderer)
+{
+    SDL_Surface *text_surface;
+    SDL_Texture *text_texture;
+    SDL_Rect solidRect;
+
+    solidRect.x = x;
+    solidRect.y = y;
+    // create a surface from the string text with a predefined font
+    text_surface = TTF_RenderText_Blended(_font,text,*_color);
+    if(!text_surface)
+    {
+        printf("TTF_RenderText_Blended: %s\n", TTF_GetError());
+        exit(EXIT_FAILURE);
+    }
+    // create texture
+    text_texture = SDL_CreateTextureFromSurface(_renderer, text_surface);
+    // obtain size
+    SDL_QueryTexture( text_texture, NULL, NULL, &solidRect.w, &solidRect.h );
+    // render it !
+    SDL_RenderCopy(_renderer, text_texture, NULL, &solidRect);
+    // clear memory
+    SDL_DestroyTexture(text_texture);
+    SDL_FreeSurface(text_surface);
+    return solidRect.h;
+}
+
+/**
+ * InitEverything: Initializes the SDL2 library and all graphical components: font, window, renderer
+ * \param width width in px of the window
+ * \param height height in px of the window
+ * \param _font font that will be used to render the text
+ * \param _img surface to be created with the table background and IST logo
+ * \param _window represents the window of the application
+ * \param _renderer renderer to handle all rendering in a window
+ */
+void InitEverything(int width, int height, TTF_Font **_font, SDL_Surface *_img[], SDL_Window** _window, SDL_Renderer** _renderer)
+{
+    InitSDL();
+    InitFont();
+    *_window = CreateWindow(width, height);
+    *_renderer = CreateRenderer(width, height, *_window);
+
+    // load the table texture
+    _img[0] = IMG_Load("table_texture.png");
+    if (_img[0] == NULL)
+    {
+        printf("Unable to load image: %s\n", SDL_GetError());
+        exit(EXIT_FAILURE);
+    }
+
+    // load IST logo
+    _img[1] = SDL_LoadBMP("ist_logo.bmp");
+    if (_img[1] == NULL)
+    {
+        printf("Unable to load bitmap: %s\n", SDL_GetError());
+        exit(EXIT_FAILURE);
+    }
+    // this opens (loads) a font file and sets a size
+    *_font = TTF_OpenFont("FreeSerif.ttf", 16);
+    if(!*_font)
+    {
+        printf("TTF_OpenFont: %s\n", TTF_GetError());
+        exit(EXIT_FAILURE);
+    }
+}
+
+/**
+ * InitSDL: Initializes the SDL2 graphic library
+ */
+void InitSDL()
+{
+    // init SDL library
+    if ( SDL_Init( SDL_INIT_EVERYTHING ) == -1 )
+    {
+        printf(" Failed to initialize SDL : %s\n", SDL_GetError());
+        exit(EXIT_FAILURE);
+    }
+}
+
+/**
+ * InitFont: Initializes the SDL2_ttf font library
+ */
+void InitFont()
+{
+    // Init font library
+    if(TTF_Init()==-1)
+    {
+        printf("TTF_Init: %s\n", TTF_GetError());
+        exit(EXIT_FAILURE);
+    }
+}
+
+/**
+ * CreateWindow: Creates a window for the application
+ * \param width width in px of the window
+ * \param height height in px of the window
+ * \return pointer to the window created
+ */
+SDL_Window* CreateWindow(int width, int height)
+{
+    SDL_Window *window;
+    // init window
+    window = SDL_CreateWindow( "1024", WINDOW_POSX, WINDOW_POSY, width, height, 0 );
+    // check for error !
+    if ( window == NULL )
+    {
+        printf("Failed to create window : %s\n", SDL_GetError());
+        exit(EXIT_FAILURE);
+    }
+    return window;
+}
+
+/**
+ * CreateRenderer: Creates a renderer for the application
+ * \param width width in px of the window
+ * \param height height in px of the window
+ * \param _window represents the window for which the renderer is associated
+ * \return pointer to the renderer created
+ */
+SDL_Renderer* CreateRenderer(int width, int height, SDL_Window *_window)
+{
+    SDL_Renderer *renderer;
+    // init renderer
+    renderer = SDL_CreateRenderer( _window, -1, 0 );
+
+    if ( renderer == NULL )
+    {
+        printf("Failed to create renderer : %s", SDL_GetError());
+        exit(EXIT_FAILURE);
+    }
+
+    // set size of renderer to the same as window
+    SDL_RenderSetLogicalSize( renderer, width, height );
+
+    return renderer;
+}
